85a66051e58830e946c4733b69a19aac745e30a5
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-02-26 17:29:19 -0500

    Tak out try catch

--- src/API.js
+++ src/API.js
@@ -1,14 +1,11 @@
 const Weather = (() => {
     
     const getCurrentWeather = async (city) => {
-        try{const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=fbd1bb439cf94a23a91212508232402&q=${city}&aqi=yes`, {
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=fbd1bb439cf94a23a91212508232402&q=${city}&aqi=yes`, {
             mode: 'cors'
         })
         const currentWeather = await response.json()
-        console.log(currentWeather)}
-        catch (error){
-            console.log(error)
-        }
+        return currentWeather
     }
 
     return getCurrentWeather
