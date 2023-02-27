cf66e1f37f714f885e15c76fb7ad3b967afc54b6
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-02-26 21:36:31 -0500

    add background img

--- src/render.js
+++ src/render.js
@@ -1,40 +1,50 @@
 import Weather from "./API";
+import helperFunction from "./helper_fnc";
 
 const renderWeather = (() => {
+    const search = document.querySelector('input')
+    const searchBtn = document.querySelector('.search_btn')
     const cityName = document.querySelector('#city_name')
     const countryName = document.querySelector('#country')
     const date = document.querySelector('#date')
+    const time = document.querySelector('#time')
     const temperature = document.querySelector('#temperature')
     const scale = document.querySelector('#temperature_scale')
-    const img = document.querySelector('img')
+    const img = document.querySelector('.weather_icon')
     const details = document.querySelector('#weather')
     const feelsLike = document.querySelector('#feels_like')
     const humidity = document.querySelector('#humidity')
     const windSpeed = document.querySelector('#wind_speed')
     const airQuality = document.querySelector('#air_quality')
+    const errorSearch = document.querySelector('.error')
 
-    const getURL = (url) => {
-        const numberPNG = url.split('/').pop()
-        const number = numberPNG.split('.')[0] 
-        return number
-    }
+
+
+
+    
+
 
     const domData = (weather) => {
         cityName.textContent = weather.location.name
         countryName.textContent = weather.location.country
-        date.textContent = weather.location.localtime
+        date.textContent = helperFunction.formatDate(weather.location.localtime)
+        time.textContent = helperFunction.formatTime(weather.location.localtime)
         if(scale.className === 'celsius'){
             temperature.textContent = `${weather.current.temp_c} °C`
         }
         if(scale.className === 'fahrenheit'){
             temperature.textContent = `${weather.current.temp_f} °F`
         }
-        const number = getURL(weather.current.condition.icon)
+        const number = helperFunction.getURL(weather.current.condition.icon)
+        const dayURL = String(`../background_imgs/day_imgs/${helperFunction.getRandomNumber()}.jpg`)
+        const nightURL = `../background_imgs/night_imgs/${helperFunction.getRandomNumber()}.jpg`
         if(weather.current.is_day === 0){
             img.src = `../weather_icons/night_weather/${number}.svg`
+            document.body.style.backgroundImage = `url(${nightURL})`
         }
         if(weather.current.is_day === 1){
             img.src = `../weather_icons/day_weather/${number}.svg`
+            document.body.style.backgroundImage = `url(${dayURL})`
         }
         details.textContent = weather.current.condition.text
         if(scale.className === 'celsius'){
@@ -42,7 +52,7 @@ const renderWeather = (() => {
             windSpeed.nextElementSibling.textContent = `${weather.current.wind_kph} kph`
         }
         if(scale.className === 'fahrenheit'){
-            feelsLike.nextElementSibling.textContent = `${weather.currentWeather.feelslike_f} °F`
+            feelsLike.nextElementSibling.textContent = `${weather.current.feelslike_f} °F`
             windSpeed.nextElementSibling.textContent = `${weather.current.wind_mph} mph`
         }
         humidity.nextElementSibling.textContent = `${weather.current.humidity}%`
@@ -72,11 +82,67 @@ const renderWeather = (() => {
     }
 
 
-    const renderPage = async(city) => {
-        const weatherForecast = await Weather(city)
-        domData(weatherForecast)
+    const tempMetric = () => {
+        let temp
+        let fLTemp
+        let wS
+        if(scale.className === 'celsius'){
+            scale.classList.add('fahrenheit')
+            scale.classList.remove('celsius')
+            scale.textContent ='Display: °F'
+            temp = temperature.textContent
+            fLTemp = feelsLike.nextElementSibling.textContent
+            wS = windSpeed.nextElementSibling.textContent
+            const cTemp = helperFunction.CToF(temp)
+            const cFLTemp = helperFunction.CToF(fLTemp)
+            const ktoM = helperFunction.kToM(wS)
+            temperature.textContent = cTemp
+            feelsLike.nextElementSibling.textContent = cFLTemp
+            windSpeed.nextElementSibling.textContent = ktoM
+        }
+        
+        else{
+            scale.classList.add('celsius')
+            scale.classList.remove('fahrenheit')
+            scale.textContent ='Display: °C'
+            temp = temperature.textContent
+            fLTemp = feelsLike.nextElementSibling.textContent
+            wS = windSpeed.nextElementSibling.textContent
+            const fTemp = helperFunction.FToC(temp)
+            const fFLTemp = helperFunction.FToC(fLTemp)
+            const mtoK = helperFunction.mToK(wS)
+            temperature.textContent = fTemp
+            feelsLike.nextElementSibling.textContent = fFLTemp
+            windSpeed.nextElementSibling.textContent = mtoK
+        }
+ 
     }
-    return renderPage
+
+    const renderPage = async(city) => {
+        try{
+            const weatherForecast = await Weather(city)
+            domData(weatherForecast) 
+        }catch(error){
+            if(city === ''){
+                errorSearch.textContent = 'Please input a city'
+            }
+            else{
+                errorSearch.textContent = 'City not found'
+            }
+        }
+       
+        scale.addEventListener('click', tempMetric)
+         
+    }
+
+    const createWeatherAPP = async() => {
+        searchBtn.addEventListener('click', async() => {
+            renderPage(search.value)
+            errorSearch.textContent = ''
+        })
+    }
+
+    return {renderPage,createWeatherAPP}
     
 })()
 
